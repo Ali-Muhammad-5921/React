76fc4cb8de9abed175de54709d4a3df183e9a050
Ali-Muhammad-5921 <alimuhammad5921@gmail.com>
2025-05-01 11:22:02 +0500

    edit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+        <h1>ASLAM O ALIKUM</h1> 
+        <hr />
+        <h2>My Name is Ali Muhammad</h2>
+        <hr />
+        <h3>This is my first Vite React Project</h3>
+
+
+      </div> 
+    </>
+  );
+}
+
+export default App
